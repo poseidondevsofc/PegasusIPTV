3dfc9f3fc10c3f77c3661c75b2b31932b4000f70
Poseidon Devs <poseidondevs.ofc@gmail.com>
2025-10-20 15:10:40 -0300

    Update db.js

--- api/db.js
+++ api/db.js
@@ -1,10 +1,9 @@
-import { Pool } from 'pg';
+import pkg from 'pg';
+const { Pool } = pkg;
 
-const pool = new Pool({
+export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false,
   },
 });
-
-export default pool;

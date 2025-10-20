389b3f0a1a8e93fa73656777e376456164c6119c
Poseidon Devs <poseidondevs.ofc@gmail.com>
2025-10-20 12:53:20 -0300

    Add files via upload

---
+++ db.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const DB_FILE = './db.json';
+function readDB() {
+  try { return JSON.parse(fs.readFileSync(DB_FILE, 'utf8') || '[]'); }
+  catch (e) { return []; }
+}
+function writeDB(data) {
+  fs.writeFileSync(DB_FILE, JSON.stringify(data, null, 2), 'utf8');
+}
+module.exports = { readDB, writeDB };
---
+++ index.js
@@ -0,0 +1,1 @@
+console.log('Pegasus IPTV backend');

bca76c5e94e69560a98482e28c7069d7df4e5607
Poseidon Devs <poseidondevs.ofc@gmail.com>
2025-10-23 17:14:33 -0300

    Delete api directory

--- api/index.js
+++
@@ -1,5 +0,0 @@
-// api/index.js - info endpoint
-module.exports = (req, res) => {
-  if (req.method === 'GET') return res.json({ ok: true, api: 'Pegasus IPTV (serverless)' });
-  res.status(405).send('Method Not Allowed');
-};
